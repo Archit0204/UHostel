e1b3db605f9c0bca235df718de8bf7bd51cfd9e7
Archit Mittal <amittal0204@gmail.com>
2025-02-08 00:25:18 +0530

    color vars

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -16,7 +16,10 @@ export default {
         customGray: "#E5E7EB",
         customDarkGray: "#6B7280",
         customDarkBlue: "#1565d8",
-        customBlue: "#007bff"
+        customBlue: "#007bff",
+        customMidGray: "#B5B5B5",
+        customGreen: "#339900",
+        customTextGray: "#545454"
       },
     },
   },
