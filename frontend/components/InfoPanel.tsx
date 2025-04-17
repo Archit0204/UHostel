882fcc4793b83e16058bdf3d3e8de4671cad4961
Archit0204 <amittal0204@gmail.com>
2025-04-17 18:09:43 +0000

    minor restructuring

--- frontend/components/InfoPanel.tsx
+++ frontend/components/InfoPanel.tsx
@@ -1,52 +1,21 @@
 import Image from "next/image";
-import profile from "@/public/profile.jpg";
+import user from "@/public/user.png";
 import logout from "@/public/logout-red.png";
-import axios, { isAxiosError } from "axios";
+import { Student } from "@/lib/types";
 
-type Cookie = {
-    name: string,
-    value: string
-};
-
-export default async function InfoPanel({ token }: { token: Cookie }) {
-
-    let studentData = null;
-    try {
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/student/user`, {
-            headers: {
-                "Authorization": `Bearer ${token?.value}`
-            },
-            withCredentials: true
-        });
-
-        studentData = response.data.studentData;
-    } catch (error: any) {
-        console.log("Error fetching user data:", error);
-
-        if (isAxiosError(error)) {
-            console.log(error.response?.data);
-            
-        }
-        
-        // fallack UI
-        return (
-            <div>
-                Error Fetching Data
-            </div>
-        )
-    }
+export default async function InfoPanel({ student }: { student: Student }) {
 
     return (
         <div className="flex w-full items-center rounded-md uppercase justify-between border border-gray-200 border-l-4 border-l-customRed bg-white py-4 px-6 shadow-md">
             {/* Profile & Info Section */}
             <div className="flex items-center gap-4">
-                <Image src={profile} alt="Profile" className="rounded-full" width={55} height={55} />
+                <Image src={user} alt="Profile" className="rounded-full" width={55} height={55} />
                 <div className="text-customTextGray">
-                <h4 className="text-base font-semibold">Hi, {studentData.firstName} {studentData.lastName}</h4>
+                <h4 className="text-base font-semibold">Hi, {student.firstName} {student.lastName}</h4>
                 <div className="text-sm text-customTextGray font-medium leading-5 uppercase">
-                    <p>{studentData.fatherName}</p>
-                    <p>{studentData.username}</p>
-                    <p>{studentData.year}-{studentData.campus}-{studentData.course}</p>
+                    <p>{student.fatherName}</p>
+                    <p>{student.username}</p>
+                    <p>{student.year}-{student.campus}-{student.course}</p>
                 </div>
                 </div>
             </div>
---
+++ frontend/lib/types.ts
@@ -0,0 +1,10 @@
+
+export type Student = {
+    firstName: string,
+    lastName: string,
+    fatherName: string,
+    username: string,
+    year: string,
+    campus: string,
+    course: string
+};
